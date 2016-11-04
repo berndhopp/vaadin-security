1607f12ea2845ac4227eadc4270b22e4258a5979
berndhopp <bernd@vaadin.com>
2016-11-04 12:43:07 +0100

    Restricted may be applied on view

--- src/main/java/org/vaadin/security/impl/SecureViewChangeListener.java
+++ src/main/java/org/vaadin/security/impl/SecureViewChangeListener.java
@@ -1,36 +1,48 @@
 package org.vaadin.security.impl;
 
+import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.UI;
 
+import org.vaadin.security.annotation.Restricted;
+import org.vaadin.security.api.PermissionEvaluator;
 import org.vaadin.security.api.SecureView;
 
 public class SecureViewChangeListener implements ViewChangeListener {
 
     private final String permissionDeniedTarget;
+    private final PermissionEvaluator permissionEvaluator;
 
-    public SecureViewChangeListener(String permissionDeniedTarget) {
+    public SecureViewChangeListener(String permissionDeniedTarget, PermissionEvaluator permissionEvaluator) {
         if(permissionDeniedTarget == null) throw new IllegalArgumentException("permissionDeniedTarget cannot be null or empty");
+        if(permissionEvaluator == null) throw new IllegalArgumentException("permissionEvaluator cannot be null");
         this.permissionDeniedTarget = permissionDeniedTarget;
-    }
-
-    public SecureViewChangeListener(){
-        this.permissionDeniedTarget = "";
+        this.permissionEvaluator = permissionEvaluator;
     }
 
     public boolean beforeViewChange(ViewChangeEvent event) {
 
-        if (event.getNewView() instanceof SecureView) {
-            boolean canAccess = ((SecureView) event.getNewView()).canAccess(event.getParameters());
+        final View newView = event.getNewView();
 
-            if (!canAccess) {
-                UI.getCurrent().getNavigator().navigateTo(permissionDeniedTarget);
-            }
+        boolean canAccess = true;
 
-            return canAccess;
+        if (newView instanceof SecureView) {
+            canAccess = ((SecureView) newView).canAccess(event.getParameters());
         } else {
-            return true;
+            final Class<? extends View> viewClass = newView.getClass();
+
+            final Restricted annotation = viewClass.getAnnotation(Restricted.class);
+
+            if(annotation != null){
+                canAccess = permissionEvaluator.hasPermission(annotation.value());
+            }
         }
+
+        if (!canAccess) {
+            UI.getCurrent().getNavigator().navigateTo(permissionDeniedTarget);
+        }
+
+        return canAccess;
     }
 
     public void afterViewChange(ViewChangeEvent event) {
