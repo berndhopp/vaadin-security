d817d9329e348e9e68106ef277dcb281d8a940f9
berndhopp <bernd@vaadin.com>
2017-04-12 15:54:59 +0200

    documentation

--- src/main/java/org/ilay/DataReverter.java
+++ src/main/java/org/ilay/DataReverter.java
@@ -5,6 +5,8 @@ import com.vaadin.data.HasDataProvider;
 import java.lang.ref.Reference;
 import java.util.Objects;
 
+//reverts all operations on 'data' ( instead of 'objects' ), like
+//Authorization.restrictData()
 class DataReverter extends OneTimeUsableReverter {
 
     private final Reference<HasDataProvider> hasDataProvider;
