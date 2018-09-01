90ba124d2858e69446b1cf67eef7ba1a61f54baf
berndhopp <berndjhopp@gmail.com>
2018-09-01 13:29:29 +0200

    removed actions

--- core/src/main/java/org/ilay/actions/ActionGuard.java
+++
@@ -1,7 +0,0 @@
-package org.ilay.actions;
-
-import java.lang.annotation.Annotation;
-
-public interface ActionGuard<ANNOTATION extends Annotation> {
-    void ifAllowed(ANNOTATION annotation, Runnable runnable);
-}
