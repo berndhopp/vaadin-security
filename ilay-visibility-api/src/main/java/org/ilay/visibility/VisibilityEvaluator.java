e8913b0ec923c48d2eab38e2a8e1e68be4b5e411
berndhopp <berndjhopp@gmail.com>
2018-10-29 15:30:18 +0100

    code-reformat

--- ilay-visibility-api/src/main/java/org/ilay/visibility/VisibilityEvaluator.java
+++ ilay-visibility-api/src/main/java/org/ilay/visibility/VisibilityEvaluator.java
@@ -1,7 +1,10 @@
 package org.ilay.visibility;
 
-import java.lang.annotation.Annotation;
+public interface VisibilityEvaluator<PAYLOAD> {
 
-public interface VisibilityEvaluator<ANNOTATION extends Annotation> {
-    boolean evaluateVisibility(ANNOTATION annotation);
+    default boolean evaluateVisibility() {
+        return evaluateVisibility(null);
+    }
+
+    boolean evaluateVisibility(PAYLOAD PAYLOAD);
 }
