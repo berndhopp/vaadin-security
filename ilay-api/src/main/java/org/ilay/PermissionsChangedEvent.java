d1f9478db691fbff152f6bb2ffb1848904e0a01f
berndhopp <berndjhopp@gmail.com>
2018-11-13 10:37:28 +0100

    PermissionChangedEvent missing from last commit, constructor is now private and fire()-method introduced for convenience

---
+++ ilay-api/src/main/java/org/ilay/PermissionsChangedEvent.java
@@ -0,0 +1,26 @@
+package org.ilay;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.UI;
+
+/**
+ * Fired when permissions have changed. This is usually the case on login/logout, or when the
+ * current user has been granted or revoked certain permissions.
+ *
+ * So whenever permissions change, it is important to call this line of code:
+ * <code>
+ * ComponentUtil.fireEvent(UI.getCurrent(), new PermissionsChangedEvent());
+ * </code>
+ */
+public class PermissionsChangedEvent extends ComponentEvent<UI> {
+    private static final long serialVersionUID = -668093721644293986L;
+
+    private PermissionsChangedEvent() {
+        super(UI.getCurrent(), false);
+    }
+
+    public static void fire() {
+        ComponentUtil.fireEvent(UI.getCurrent(), new PermissionsChangedEvent());
+    }
+}
